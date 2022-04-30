16f4820d063773190cce705d58c3a8718fa2b2d0
jootnet <shawryan@outlook.com>
2022-04-30 15:44:59 +0800

    split common functions

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ powered by io.netty
 ## 2022-4-17 first commit
 
 ## 2022-4-19 add login logic
+
+split common functions
--- core/src/main/java/joot/m2/server/M2WebSocketServerHandler.java
+++ core/src/main/java/joot/m2/server/M2WebSocketServerHandler.java
@@ -1,5 +1,10 @@
 package joot.m2.server;
 
+import com.github.jootnet.m2.core.net.Messages;
+import com.github.jootnet.m2.core.net.messages.LoginReq;
+import com.github.jootnet.m2.core.net.messages.LoginResp;
+
+import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.group.ChannelGroup;
@@ -7,9 +12,6 @@ import io.netty.channel.group.DefaultChannelGroup;
 import io.netty.handler.codec.http.websocketx.BinaryWebSocketFrame;
 import io.netty.handler.codec.http.websocketx.WebSocketFrame;
 import io.netty.util.concurrent.GlobalEventExecutor;
-import joot.m2.server.net.Messages;
-import joot.m2.server.net.messages.LoginReq;
-import joot.m2.server.net.messages.LoginResp;
 
 public final class M2WebSocketServerHandler extends SimpleChannelInboundHandler<WebSocketFrame> {
 	// 所有人物 FIXME 改为地图/区域等
@@ -20,13 +22,13 @@ public final class M2WebSocketServerHandler extends SimpleChannelInboundHandler<
     	
         if (frame instanceof BinaryWebSocketFrame) {
         	try {
-        		var msg = Messages.unpack(frame.content());
+        		var msg = Messages.unpack(frame.content().array());
         		switch (msg.type()) {
         		
         		case HUM_ACTION_CHANGE: {
         			// TODO 校验地图/人物/npc/怪物重合
         			// 广播人物动作更改
-        			allChannelGroup.writeAndFlush(new BinaryWebSocketFrame(Messages.pack(msg)));
+        			allChannelGroup.writeAndFlush(new BinaryWebSocketFrame(Unpooled.wrappedBuffer(Messages.pack(msg))));
         			break;
         		}
         		case LOGIN_REQ: {
@@ -47,14 +49,14 @@ public final class M2WebSocketServerHandler extends SimpleChannelInboundHandler<
         						var roles = new LoginResp.Role[1];
         						roles[0] = new LoginResp.Role();
         						roles[0].name = i == 0 ? "AlexKit" : i == 1 ? "二条" : "林星";
-        						ctx.channel().writeAndFlush(new BinaryWebSocketFrame(Messages.pack(new LoginResp(0, null, roles))));
+        						ctx.channel().writeAndFlush(new BinaryWebSocketFrame(Unpooled.wrappedBuffer(Messages.pack(new LoginResp(0, null, roles)))));
         						return;
         					}
-    						ctx.channel().writeAndFlush(new BinaryWebSocketFrame(Messages.pack(new LoginResp(1, null, null))));
+    						ctx.channel().writeAndFlush(new BinaryWebSocketFrame(Unpooled.wrappedBuffer(Messages.pack(new LoginResp(1, null, null)))));
     						return;
         				}
         			}
-					ctx.channel().writeAndFlush(new BinaryWebSocketFrame(Messages.pack(new LoginResp(2, null, null))));
+					ctx.channel().writeAndFlush(new BinaryWebSocketFrame(Unpooled.wrappedBuffer(Messages.pack(new LoginResp(2, null, null)))));
         			break;
         		}
 				default:
